3492c2d04c717d09278a44ba14c34b85bd76ccb1
erikyoung <erik.young@rmit.edu.vn>
2017-07-20 01:02:09 +0700

    corrected error message flash

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,6 @@ class CommentsController < ApplicationController
 	def create	
 		@teacher = Teacher.find(params[:teacher_id])
 		@comment = @teacher.comments.create(comment_params.merge(user: current_user))
-		@comment.user_id = current_user
 		if @comment.save
 			flash[:success] = "New Comment posted!"
 			redirect_to teacher_path(@teacher)
