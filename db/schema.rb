5113249b78cfbb0115e550dee7d2cd7c83dd20c0
Hai Minh Do <dhminh1024@gmail.com>
2017-07-19 16:45:08 +0700

    Fix some bug and add video walkthrough

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170718175002) do
+ActiveRecord::Schema.define(version: 20170718182103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-
-  create_table "comments", force: :cascade do |t|
-    t.text "message"
-    t.integer "rating"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "teacher_id"
 
   create_table "attachments", force: :cascade do |t|
     t.string "attachment"
@@ -34,7 +24,15 @@ ActiveRecord::Schema.define(version: 20170718175002) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["attached_item_id", "attached_item_type"], name: "index_attachments_on_attached_item_id_and_attached_item_type"
+  end
 
+  create_table "comments", force: :cascade do |t|
+    t.text "message"
+    t.integer "rating"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "teacher_id"
   end
 
   create_table "follows", force: :cascade do |t|
