be250b0e34babf998b2b4c31d1960354d778ba07
Hai Minh Do <dhminh1024@gmail.com>
2017-07-30 06:03:42 +0700

    Admin review denied corrections and decide

--- app/controllers/corrections_controller.rb
+++ app/controllers/corrections_controller.rb
@@ -3,10 +3,15 @@ class CorrectionsController < ApplicationController
   before_action :require_teacher
 
   def index
-    @corrections = current_user.corrections
+    if current_user.is_admin?
+      @corrections = Correction.where(status: [Correction::STATUS_DENIED, Correction::STATUS_DENY_CONFIRMED, Correction::STATUS_DENY_REFUSED])
+    else
+      @corrections = current_user.corrections
+    end
   end
 
   def show
+    @correction = Correction.find_by(id: params[:id])
   end
 
   def edit
@@ -20,7 +25,7 @@ class CorrectionsController < ApplicationController
       elsif params[:finish].present?
         @correction.update_attribute(:status, 'done')
         # send message
-        @message = Message.new(sender: current_user.id, 
+        @message = Message.new(sender: current_user.id,
           subject: 'Your writing has been finished',
           content: 'Teacher has been finish correcting your writing',
           is_read: false,
@@ -39,7 +44,7 @@ class CorrectionsController < ApplicationController
         flash[:success] = "You have finish your corrections"
         redirect_to corrections_path
       end
-    else 
+    else
       flash[:error] = @correction.errors.full_messages.to_sentence
       render :edit
     end
@@ -50,7 +55,25 @@ class CorrectionsController < ApplicationController
       return render :edit
   end
 
-  private 
+  def accept_deny_correction
+    @correction = Correction.find_by(id: params[:id])
+    @correction.status = Correction::STATUS_DENY_CONFIRMED
+    flash[:success] = "Correction deny accepted!"
+    @correction.save
+    Account.cancel_payment_for_correcting(@correction.get_user_student.account.id, @correction.get_teacher_account.id, @correction.get_teacher.price, @correction.writing.id)
+    return redirect_to corrections_path
+  end
+
+  def refuse_deny_correction
+    @correction = Correction.find_by(id: params[:id])
+    @correction.status = Correction::STATUS_DENY_REFUSED
+    flash[:success] = "Correction deny refused!"
+    @correction.save
+    Account.settle_payment_for_correcting(@correction.get_user_student.account.id, @correction.get_teacher_account.id, @correction.get_teacher.price, @correction.writing.id)
+    return redirect_to corrections_path
+  end
+
+  private
   def set_correction
     @correction = Correction.find(params[:id])
     @writing = @correction.writing
