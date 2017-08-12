e59da1885d81c68b275d7d58ddf20fffa5483d76
Toan Nguyen <n.minhtoan@gmail.com>
2017-08-12 13:17:28 +0700

    fix bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,7 @@ class User < ApplicationRecord
   # functions
 
   def name_or_username
-    if user.respond_to?(:name) && self.name.present?
+    if self.respond_to?(:name) && self.name.present?
       return self.name
     else
       return self.username
